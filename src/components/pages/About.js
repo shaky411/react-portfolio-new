493b85eabd500700d7b0c3f83a59b7bcce3dd425
Marc Norris <marcnorris007@icloud.com>
2023-03-03 15:51:45 +0000

    updated app folder structure

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,41 @@
 import React from "react";
+import myLogo from "/Users/marc/repos/react-portfolio-new/src/assets/user-mjn.jpg"
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
+            Hi, I'm Marc.
+            <br className="hidden lg:inline-block" />I love to build amazing
+            apps with React.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Please join me on my journey into the world of Front End Development and check out my current and past projects below.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded hover:shadow-md text-lg">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded hover:shadow-md text-lg">
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-sm lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded-xl shadow-xl"
+            alt="hero"
+            src={myLogo}
+          />
+        </div>
+      </div>
+    </section>
   );
 }
 
