d56a8cc610237e8c8930436fb257a1aa72c10342
Marc Norris <marcnorris007@icloud.com>
2023-03-03 21:01:40 +0000

    test

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -48,7 +48,7 @@ function NavTabs() {
       </li>
       <li className="nav-item">
         <NavLink
-          to="/react-porfolio-new/contact/learn"
+          to="/react-portfolio-new/contact/learn"
           className={({ isActive }) =>
             isActive ? 'nav-link active' : 'nav-link'
           }
