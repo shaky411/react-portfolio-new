01e7d0f576223d3172e153f86b51731f410646c9
Marc Norris <marcnorris007@icloud.com>
2023-03-06 20:25:10 +0000

    project cards now pull data from JSON

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -14,17 +14,17 @@ window.onresize = showBrowserWidth
 
 export default function ProjectCard(props) {
   return (
-    <div className="flex flex-col p-4 m-3 w-1/1 md:w-1/2 lg:w-1/3 xl:w-1/4 space-y-10 bg-white rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
+    <div className="flex flex-col p-4 m-3 w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/4 space-y-10 bg-white rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
       <div className="flex flex-col md:flex-col lg:flex-col space-y-6">
         <div className="flex justify-center items-center flex-col space-y-3 text-center md:text-left">
-          <img src={pLogo1} alt="test" className="w-1/1 shadow-md rounded-m" />
+          <img src={props.image} alt="test" className="w-1/1 shadow rounded" />
 
           <div className="max-w-sm mt-4 text-2xl font-medium">
-            {props.projectSub}
+            {props.tech}
           </div>
 
           <div className="flex-col mb-4 spacy-y-3 text-center md:text-left">
-            <p className="text-5xl font-bold">{props.title}</p>
+            <p className="text-5xl font-bold">{props.name}</p>
             <p className="text-sm font-light text-gray-400">{props.description}</p>
             <div>
               <div className="inline-block px-3 mt-2 py-1 text-sm text-white font-bold bg-slate-500 rounded-full shadow-md">
@@ -33,7 +33,7 @@ export default function ProjectCard(props) {
               </div>
               <div>
                   <a
-                    href="https://github.com/shaky411"
+                    href={props.github}
                     target="_blank"
                     className="text-black"
                   ><i className="fa-brands fa-square-github text-2xl mt-2 hover:text-indigo-500"></i></a>
