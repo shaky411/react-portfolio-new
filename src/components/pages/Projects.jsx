56e9d20dfc5c568ee5c8dac5759ad25f777dc88a
Marc Norris <marcnorris007@icloud.com>
2023-03-07 09:26:56 +0000

    added live page links

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -21,12 +21,12 @@ function Projects() {
 
     {/* Project Cards */}
       <div className="flex flex-wrap items-center rounded-2xl justify-center p-2 bg-slate-100">
-      <ProjectCard pill={pill} tech={projects[0].tech} description={projects[0].description} image={projects[0].image} name={projects[0].name} github={projects[0].github}/>
-      <ProjectCard pill={pill} tech={projects[1].tech} description={projects[1].description} image={projects[1].image} name={projects[1].name} github={projects[1].github}/>
-      <ProjectCard pill={pill} tech={projects[2].tech} description={projects[2].description} image={projects[2].image} name={projects[2].name} github={projects[2].github}/>
-      <ProjectCard pill={pill} tech={projects[3].tech} description={projects[3].description} image={projects[3].image} name={projects[3].name} github={projects[3].github}/>
-      <ProjectCard pill={pill} tech={projects[4].tech} description={projects[4].description} image={projects[4].image} name={projects[4].name} github={projects[4].github}/>
-      <ProjectCard pill={pill} tech={projects[5].tech} description={projects[5].description} image={projects[5].image} name={projects[5].name} github={projects[5].github}/>
+      <ProjectCard pill={pill} tech={projects[0].tech} description={projects[0].description} image={projects[0].image} name={projects[0].name} github={projects[0].github} live={projects[0].live}/>
+      <ProjectCard pill={pill} tech={projects[1].tech} description={projects[1].description} image={projects[1].image} name={projects[1].name} github={projects[1].github} live={projects[1].live}/>
+      <ProjectCard pill={pill} tech={projects[2].tech} description={projects[2].description} image={projects[2].image} name={projects[2].name} github={projects[2].github} live={projects[2].live}/>
+      <ProjectCard pill={pill} tech={projects[3].tech} description={projects[3].description} image={projects[3].image} name={projects[3].name} github={projects[3].github} live={projects[3].live}/>
+      <ProjectCard pill={pill} tech={projects[4].tech} description={projects[4].description} image={projects[4].image} name={projects[4].name} github={projects[4].github} live={projects[4].live}/>
+      <ProjectCard pill={pill} tech={projects[5].tech} description={projects[5].description} image={projects[5].image} name={projects[5].name} github={projects[5].github} live={projects[5].live}/>
       
       
       </div>
